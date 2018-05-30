4bd7391ea2cb4296ff4b798dbcbc81d1f3a7aed9
Grzegorz Sobczyk <grzegorzsobczykdev@gmail.com>
2018-05-30 16:15:36 +0200

    Add Routes and Contact Home components

--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ import { Provider } from 'react-redux';
 import store from './store';
 import DevTools from './DevTools';
 import {getCountries} from './actions/actions';
-
+import { Router, hashHistory } from 'react-router';
+import routes from './routes';
 
 render(
     <Provider store={store}>
-        <div>
-            <h1>Inicjalizcja projektu</h1>
-            <DevTools />
-        </div>
+            <div>
+                <Router history={hashHistory} routes={routes} />
+                <DevTools />
+            </div>
     </Provider>,
     document.getElementById('root')
 );
