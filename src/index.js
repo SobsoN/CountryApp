2fafde7012a52c73aa42234fe56562f9717454e9
Grzegorz Sobczyk <grzegorzsobczykdev@gmail.com>
2018-05-30 16:29:01 +0200

    Finish routing, Add styles

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import DevTools from './DevTools';
-import {getCountries} from './actions/actions';
+//import DevTools from './DevTools';
+//import {getCountries} from './actions/actions';
+//<DevTools />
 import { Router, hashHistory } from 'react-router';
 import routes from './routes';
-
+import './country.css'
 render(
     <Provider store={store}>
             <div>
                 <Router history={hashHistory} routes={routes} />
-                <DevTools />
+              
             </div>
     </Provider>,
     document.getElementById('root')
 );
-store.dispatch(getCountries());
